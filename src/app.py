fa36a4ab8cb7da6fd8f2dc97861a746416530e0d
Phamishan Packiaseelan <phamishan93@gmail.com>
2025-02-25 11:13:02 +0100

    fixes
    Co-authored-by: Teis Bindner <Druumihra@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -1,2 +1,57 @@
+from PIL import Image
 import numpy as np
 import cv2
+import greyscale as gs
+import keras
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pathlib
+
+data_dir = pathlib.Path("./Images/original/")
+image_count = len(list(data_dir.glob("*")))
+print(image_count)
+
+
+
+
+# test = list(data_dir.glob(''))
+# Image.open(str(test[0]))
+
+# # Definér parametre for vores indlæser
+# batch_size = 32
+# img_height = 180
+# img_width = 180
+
+# # Valideringssplit
+# train_ds = tf.keras.utils.image_dataset_from_directory(
+#   data_dir,
+#   validation_split=0.2,
+#   subset="training",
+#   seed=123,
+#   image_size=(img_height, img_width),
+#   batch_size=batch_size)
+
+# val_ds = tf.keras.utils.image_dataset_from_directory(
+#   data_dir,
+#   validation_split=0.2,
+#   subset="validation",
+#   seed=123,
+#   image_size=(img_height, img_width),
+#   batch_size=batch_size)
+
+# for image_batch, labels_batch in train_ds:
+#   print(image_batch.shape)
+#   print(labels_batch.shape)
+#   break
+
+# AUTOTUNE = tf.data.AUTOTUNE
+
+# train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+# val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+# normalization_layer = layers.Rescaling(1./255)
+
+# normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+# image_batch, labels_batch = next(iter(normalized_ds))
+# first_image = image_batch[0]
+# # Notice the pixel values are now in `[0,1]`.
+# print(np.min(first_image), np.max(first_image))
--- src/greyscale.py
+++ src/greyscale.py
@@ -1,4 +1,5 @@
 from PIL import Image;
+import os
 import pathlib
 
 data_dir = pathlib.Path("./")
@@ -7,21 +8,19 @@ def check_images(image_name):
     og_count = len(list(data_dir.glob('images/original/*')))
     gs_count = len(list(data_dir.glob('images/greyscaled/*')))
     if og_count != gs_count: 
-        return pathlib.Path(f"{data_dir}/greyscaled/greyscaled-{image_name}")
-    else:
-        return False
-    
+        return pathlib.Path(f"{data_dir}/greyscaled/greyscaled-{image_name}").exists()
+ 
 
 def grey_scale_images():
+    if not pathlib.Path("./images/greyscaled").exists():
+        os.mkdir("./images/greyscaled")
     for image in data_dir.glob('Images/original/*') :
-        test = image.name.split(".")
-        length = len(test)
-        if test[length-1] == "avif" :
+        image_extension_check = image.name.split(".")
+        length = len(image_extension_check)
+        if image_extension_check[length-1] == "avif" :
             return
         if check_images(image.name):
             im = Image.open(image)
             im = im.convert("L")
             image_name = image.name.split(".")
             im.save(fp=f"{data_dir}/Images/greyscaled/{image_name[0]}-greyscaled.{image_name[1]}")
-
-grey_scale_images()
